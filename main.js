960a22d3d0508ac2cf057658dd663b36b020b0dc
BrandonYuan01 <spmsby11@gmail.com>
2024-12-21 03:45:08 -0500

    test

---
+++ main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+// Define the App component that renders the Header, Main, and Footer components
+const App: React.FC = () => {
+    return (
+        <div className="App"/>
+    );
+};
+
+export default App;
