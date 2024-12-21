df56820110fdc628a83bf3dd14f2481ac15b06b7
BrandonYuan01 <spmsby11@gmail.com>
2024-12-21 17:56:31 -0500

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 // @ts-ignore
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
