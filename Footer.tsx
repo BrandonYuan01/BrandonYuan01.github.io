c6f659b5ad4e245e819a9c4a78a3dca17877730e
BrandonYuan01 <spmsby11@gmail.com>
2024-12-21 04:51:25 -0500

    test

---
+++ Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <div className="footer">
+            <p>© 2023 by My Website. All rights reserved.</p>
+        </div>
+    );
+};
