6bc8c8fe1d7247041dedd65609ade347987c6130
BrandonYuan01 <spmsby11@gmail.com>
2024-12-21 04:57:10 -0500

    test

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import Main from "./LandingPage/MainBody";
 import Header from "./LandingPage/Header";
-import Footer from "./LandingPage/Footer";
+import Footer from "./Footer";
 
 // Define the App component that renders the Header, Main, and Footer components
 const App: React.FC = () => {
